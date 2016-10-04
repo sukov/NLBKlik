84d5c2b292b415f3ec0cfcc85f0393d74098d550
WF | Gorjan Shukov <wf-gshukov@WFs-MacBook-Air.local>
2016-10-04 14:17:52 +0200

    implemented coder and decoder functions for User

--- NLBKlik/Models/User.swift
+++ NLBKlik/Models/User.swift
@@ -8,14 +8,39 @@
 
 import Foundation
 
-class User {
-	private var username: String
-	private var password: String
-	private var autoLogin: Bool
+class User: NSObject, NSCoding {
+	var username: String
+	var password: String
+	var autoLogin: Bool
 
 	init(username: String, password: String, autoLogin: Bool) {
 		self.username = username
 		self.password = password
 		self.autoLogin = autoLogin
+	}
+
+	convenience init?(userData: [String: AnyObject]) {
+		if let username = userData[UserDataKeys.username] as? String,
+			password = userData[UserDataKeys.password] as? String,
+			autoLogin = userData[UserDataKeys.autoLogin] as? Bool {
+				self.init(username: username, password: password, autoLogin: autoLogin)
+		} else {
+				return nil
+		}
+	}
+
+	required convenience init?(coder aDecoder: NSCoder) {
+		if let username = aDecoder.decodeObjectForKey(UserDataKeys.username) as? String, password = aDecoder.decodeObjectForKey(UserDataKeys.password) as? String {
+			let autoLogin = aDecoder.decodeBoolForKey(UserDataKeys.autoLogin)
+			self.init(username: username, password: password, autoLogin: autoLogin)
+		} else {
+			return nil
+		}
+	}
+
+	func encodeWithCoder(aCoder: NSCoder) {
+		aCoder.encodeObject(username, forKey: UserDataKeys.username)
+		aCoder.encodeObject(password, forKey: UserDataKeys.password)
+		aCoder.encodeBool(autoLogin, forKey: UserDataKeys.autoLogin)
 	}
 }
