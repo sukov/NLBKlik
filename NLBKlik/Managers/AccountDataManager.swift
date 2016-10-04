2cdfbea18b1e6ce7bdf52ca02834a445955f7fe5
WF | Gorjan Shukov <wf-gshukov@WFs-MacBook-Air.local>
2016-10-04 12:47:06 +0200

    rename

--- NLBKlik/Managers/AccountDataManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AccountDataManager.swift
-//  NLBKlik
-//
-//  Created by WF | Gorjan Shukov on 10/3/16.
-//  Copyright © 2016 WF | Gorjan Shukov. All rights reserved.
-//
-
-import Foundation
---
+++ NLBKlik/Managers/UserManager.swift
@@ -0,0 +1,13 @@
+//
+//  UserManager.swift
+//  NLBKlik
+//
+//  Created by WF | Gorjan Shukov on 10/4/16.
+//  Copyright © 2016 WF | Gorjan Shukov. All rights reserved.
+//
+
+import Foundation
+
+class UserManager {
+	static let sharedInstance = UserManager()
+}
