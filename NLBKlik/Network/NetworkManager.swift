9112a2a9dc3978fb6d7fd21fa49bc11d2aa5e1f5
WF | Gorjan Shukov <wf-gshukov@WFs-MacBook-Air.local>
2016-10-05 17:44:42 +0200

    changes made are still for testing

--- NLBKlik/Network/NetworkManager.swift
+++ NLBKlik/Network/NetworkManager.swift
@@ -10,11 +10,10 @@ import Foundation
 import UIKit
 
 class NetworkManager: NSObject, UIWebViewDelegate {
-
 	enum Page {
 		case Uknown
-		case FirstPage
 		case LogIn
+		case AvailableFunds
 		case Transactions
 		case ReservedFunds
 	}
@@ -22,11 +21,11 @@ class NetworkManager: NSObject, UIWebViewDelegate {
 	static let sharedInstance = NetworkManager()
 	private var webView: UIWebView = UIWebView()
 	private var pageCoordinator: Int = 0
-	private var nextPage: Page = .FirstPage
+	private var currentPage: Page = .LogIn
+	private var nextPage: Page = .Uknown
 	private var pageLoadCompleted: Optional < () -> Void >
-	private var logInCompleted: Optional < () -> Void >
-	private var transactionsLoadCompleted: Optional < () -> Void >
-	private var reservedFundsLoadCompleted: Optional < () -> Void >
+	private var logInCompleted: Optional < (success: Bool) -> Void >
+	private var loadingFinnished: Optional < () -> Void >
 
 	override init() {
 		super.init()
@@ -41,62 +40,99 @@ class NetworkManager: NSObject, UIWebViewDelegate {
 		pageCoordinator += 1
 		switch pageCoordinator {
 		case 1: pageLoadCompleted?()
-		case 2: if (nextPage == .LogIn) {
-			logInCompleted?()
+		case 2: if (nextPage == .AvailableFunds) {
+			logInCompleted?(success: true)
 			}
-		case 7, 12: if (nextPage == .Transactions) {
-			transactionsLoadCompleted?()
-			} else if (nextPage == .ReservedFunds) {
-			reservedFundsLoadCompleted?()
-			}
-		case 12: pageCoordinator -= 5
-		default: nextPage = .Uknown
+		default: break
 		}
 
 		print(pageCoordinator)
 	}
 
-	func loadWebPage(complete: ((sucess: Bool) -> Void)?) {
+	func loadWebPage(complete: ((success: Bool) -> Void)?) {
 		if (pageCoordinator > 0) {
-			complete?(sucess: true)
+			complete?(success: true)
 		} else {
+			self.webView.loadRequest(NSURLRequest(URL: NSURL(string: "https://www.nlbklik.com.mk/LoginModule/LoginUP.aspx")!))
 			pageLoadCompleted = {
-				complete?(sucess: true)
+				complete?(success: true)
 			}
 		}
 	}
 
-	func reloadWebPage(complete: (sucess: Bool) -> Void) {
+	func reloadWebPage(complete: (success: Bool) -> Void) {
 		pageCoordinator = 0
 		webView.reload()
 
 		pageLoadCompleted = {
-			complete(sucess: true)
+			complete(success: true)
 		}
 	}
 
-	func login(username: String, password: String, complete: (sucess: Bool) -> Void) {
-		nextPage = .LogIn
+	func login(username: String, password: String, complete: (success: Bool) -> Void) {
+		nextPage = .AvailableFunds
 		webView.stringByEvaluatingJavaScriptFromString("document.getElementById('ctl00_DefaultContent_UsernamePasswordLogin1_UserNameTextBox').value = '\(username)'")
 		webView.stringByEvaluatingJavaScriptFromString("document.getElementById('ctl00_DefaultContent_UsernamePasswordLogin1_PasswordTextBox').value = '\(password)'")
 
 		webView.stringByEvaluatingJavaScriptFromString("document.getElementById('ctl00_DefaultContent_UsernamePasswordLogin1_*').click()")
-
-		logInCompleted = {
-			complete(sucess: true)
+		let timer = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(checkForLoginErrors), userInfo: nil, repeats: true)
+		logInCompleted = { success in
+			timer.invalidate()
+			if (success) {
+				self.currentPage = .AvailableFunds
+			}
+			complete(success: success)
 		}
-		// NSTimer.scheduledTimerWithTimeInterval(7.5, target: self, selector: #selector(getTotal), userInfo: nil, repeats: false)
-
-		// NSTimer.scheduledTimerWithTimeInterval(8.5, target: self, selector: #selector(getRezervniSredstva), userInfo: nil, repeats: false)
 	}
 
-	func getTotal(complete: (sucess: Bool) -> Void) {
-//		pageLoaded = {
-//			self.webView.stringByEvaluatingJavaScriptFromString("document.getElementById('key_1_a_href').click()")
-//			print(self.webView.stringByEvaluatingJavaScriptFromString("document.getElementsByTagName('strong')[0].innerHTML"))
-//			complete(sucess: true)
-//		}
-		// print(total.text)
+	func getAvailableFunds(complete: (transactionAcc: [[String: String]]?, debitCards: [[String: String]]?, success: Bool) -> Void) {
+		if (currentPage != .AvailableFunds) {
+			nextPage = .AvailableFunds
+			self.webView.stringByEvaluatingJavaScriptFromString("document.getElementById('key_1_a_href').click()")
+			self.webView.stringByEvaluatingJavaScriptFromString("document.getElementById('key_3_Item').click()")
+		}
+		let timer = NSTimer.scheduledTimerWithTimeInterval(1.5, target: self, selector: #selector(checkIfLoadingIsFinnished), userInfo: nil, repeats: true)
+		loadingFinnished = {
+			timer.invalidate()
+			self.currentPage = .AvailableFunds
+			let accountsCount = Int(self.executeJavaScriptFromString("(document.getElementById('orders')).getElementsByTagName('tr').length"))!
+
+			var transactionAcc = [[String: String]]()
+			var debitCards = [[String: String]]()
+
+			var transaction: Bool = true
+			for i in 1..<accountsCount {
+				if (self.executeJavaScriptFromString("(document.getElementById('orders')).getElementsByTagName('tr')[\(i)].className") == "group") {
+					transaction = false
+				}
+
+				if (transaction) {
+					let accountName = self.executeJavaScriptFromString("(document.getElementById('orders')).getElementsByTagName('tr')[\(i)].getElementsByTagName('td')[1].getElementsByTagName('span')[0].innerHTML").stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) + "\n" + self.executeJavaScriptFromString("(document.getElementById('orders')).getElementsByTagName('tr')[\(i)].getElementsByTagName('td')[1].getElementsByTagName('i')[0].innerHTML")
+
+					guard accountName.characters.count > 4 else {
+						continue
+					}
+
+					let availableFunds = self.executeJavaScriptFromString("(document.getElementById('orders')).getElementsByTagName('tr')[\(i)].getElementsByTagName('strong')[0].innerHTML").characters.split { $0 == "&" }.map(String.init)[0]
+
+					transactionAcc.append([AccountKeys.name: accountName, AccountKeys.availableFunds: availableFunds])
+				}
+				else {
+					let cardName = self.executeJavaScriptFromString("(document.getElementById('orders')).getElementsByTagName('tr')[\(i)].getElementsByTagName('td')[1].getElementsByTagName('span')[0].innerHTML").stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) + "\n" + self.executeJavaScriptFromString("(document.getElementById('orders')).getElementsByTagName('tr')[\(i)].getElementsByTagName('td')[1].getElementsByTagName('i')[0].innerHTML")
+
+					guard cardName.characters.count > 4 else {
+						continue
+					}
+
+					let availableFunds = self.executeJavaScriptFromString("(document.getElementById('orders')).getElementsByTagName('tr')[\(i)].getElementsByTagName('strong')[1].innerHTML")
+					debitCards.append([AccountKeys.name: cardName, AccountKeys.availableFunds: availableFunds])
+				}
+				print(transactionAcc)
+				print(debitCards)
+			}
+			complete(transactionAcc: transactionAcc, debitCards: debitCards, success: true)
+		}
+		// print(self.webView.stringByEvaluatingJavaScriptFromString("document.getElementsByTagName('strong')[0].innerHTML"))
 	}
 
 	func getTransactions() {
@@ -104,9 +140,9 @@ class NetworkManager: NSObject, UIWebViewDelegate {
 
 		self.webView.stringByEvaluatingJavaScriptFromString("document.getElementById('key_21_a_href').click()")
 		self.webView.stringByEvaluatingJavaScriptFromString("document.getElementById('key_25_Item').click()")
-		// NSTimer.scheduledTimerWithTimeInterval(4.0, target: self, selector: #selector(getTableData), userInfo: nil, repeats: false)
-		transactionsLoadCompleted = {
 
+		loadingFinnished = {
+			self.currentPage = .Transactions
 		}
 	}
 
@@ -115,8 +151,9 @@ class NetworkManager: NSObject, UIWebViewDelegate {
 
 		self.webView.stringByEvaluatingJavaScriptFromString("document.getElementById('key_21_a_href').click()")
 		self.webView.stringByEvaluatingJavaScriptFromString("document.getElementById('key_29_Item').click()")
-		reservedFundsLoadCompleted = {
 
+		loadingFinnished = {
+			self.currentPage = .Transactions
 		}
 	}
 
@@ -124,6 +161,22 @@ class NetworkManager: NSObject, UIWebViewDelegate {
 
 		print(webView.stringByEvaluatingJavaScriptFromString(
 			"document.getElementById('ctl00_DefaultContent_ctl01_gvCustoms').getElementsByTagName('tr')[0].getElementsByTagName('th')[1].innerHTML"))
+	}
+
+	@objc private func checkIfLoadingIsFinnished() {
+		if (self.webView.stringByEvaluatingJavaScriptFromString(("document.getElementById('ctl00_DefaultContent_ctl00_infoDialog_mainPanel').style.display")) == "none") {
+			loadingFinnished?()
+		}
+	}
+
+	@objc private func checkForLoginErrors() {
+		if (executeJavaScriptFromString("document.getElementById('ctl00_DefaultContent_ltrMessage').innerHTML") != "") {
+			logInCompleted?(success: false)
+		}
+	}
+
+	private func executeJavaScriptFromString(string: String) -> String {
+		return webView.stringByEvaluatingJavaScriptFromString(string) ?? ""
 	}
 
 }
